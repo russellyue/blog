eb825e92e8c3db5f59aab3bd3bd29836b6070ddd
Russell Yue <russellyuecs@gmail.com>
2019-11-15 17:03:30 +0800

    add fabfile and requirement folder

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '9q_zo!mlaivy=is4zwkvn$rl$3-j+lgonfy@@k*_)2l20h3@p*'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,25 +77,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -112,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
---
+++ fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import local
+from fabric.decorators import task
+
+
+def install(requirements_env="dev"):
+    """
+    Install required packages
+    """
+    local("pip install -r requirements/%s.txt" % requirements_env)
+
+
+@task
+def run():
+    local("./manage.py runserver")
+
+
+@task
+def pycodestyle():
+    """
+    check the project for PEP8 compliance using `pycodestyle`
+    """
+    local("pycodestyle .")
+
+
+@task
+def tag_version(version):
+    """
+    Tag new version
+    """
+    local("git tag %s" % version)
+    local("git push origin %" % version)
